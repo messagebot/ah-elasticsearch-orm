bc450d9edc2538bf4ccc63035fb93529d7d0c5c4
Evan Tahler <evantahler@gmail.com>
2016-11-30 12:08:14 -0800

    string types for v5 and v2

--- test/db/elasticsearch/indexes/people.js
+++ test/db/elasticsearch/indexes/people.js
@@ -1,3 +1,12 @@
+var ES_VERSION = 5
+
+if (process.env.ES_VERSION) {
+  ES_VERSION = parseInt(process.env.ES_VERSION.split('.')[0])
+}
+
+var aggregatableStringType = 'keyword'
+if (ES_VERSION !== 5) { aggregatableStringType = 'string' }
+
 module.exports = {
   'settings': {
     'number_of_shards': parseInt(process.env.NUMBER_OF_SHARDS || 1),
@@ -7,8 +16,8 @@ module.exports = {
   'mappings': {
     'person': {
       'properties': {
-        'guid': { 'type': 'keyword', 'required': true },
-        'source': { 'type': 'keyword', 'required': true },
+        'guid': { 'type': aggregatableStringType, 'required': true },
+        'source': { 'type': aggregatableStringType, 'required': true },
         'email': {
           'type': 'string',
           'index': 'not_analyzed'
